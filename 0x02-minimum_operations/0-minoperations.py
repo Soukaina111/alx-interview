b5b2c782d4f8edd93dc8c6eed811ad96625c6f78
Soukaina111 <basmahajar@gmail.com>
2024-07-10 10:32:51 +0000

    operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,9 +4,11 @@ required to generate a string of length 'n'
 consisting entirely of 'H' characters.
 """
 
+
 def min_operations(n: int) -> int:
     """
-    Calculate the minimum operations needed to create a string of 'n' 'H' characters.
+    Calculate the minimum operations needed to
+    create a string of 'n' 'H' characters.
 
     Parameters:
     - n: An integer representing the desired length of the string.
@@ -16,19 +18,15 @@ def min_operations(n: int) -> int:
     """
     next_char = 'H'
     accumulated_string = 'H'
-    operation_count = 0
-
-    # Loop until the accumulated string reaches the target length 'n'.
-    while len(accumulated_string) < n:
+    opera = 0
+    while (len(accumulated_string) < n):
         if n % len(accumulated_string) == 0:
-            operation_count += 2
+            opera += 2
             next_char = accumulated_string
             accumulated_string += accumulated_string
         else:
-            operation_count += 1
+            opera += 1
             accumulated_string += next_char
-
     if len(accumulated_string) != n:
         return 0
-
-    return operation_count
+    return opera
